45e41161185dafc7cb9f43df746888b409ddd1e6
Des Petrov <desislavpetrov@gmail.com>
2026-06-20 17:06:47 +0100

    Update UserRepository.kt

--- services/backend/src/main/kotlin/org/dpp/tradelab/user/repository/UserRepository.kt
+++ services/backend/src/main/kotlin/org/dpp/tradelab/user/repository/UserRepository.kt
@@ -2,8 +2,10 @@ package org.dpp.tradelab.user.repository
 
 import org.dpp.tradelab.user.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.UUID
 
+@Repository
 interface UserRepository : JpaRepository<User, UUID> {
     fun existsByEmail(email: String): Boolean
 }
