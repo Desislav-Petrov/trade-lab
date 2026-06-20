bfe17f0135228987c42cd4760357107111efdd5f
Des Petrov <desislavpetrov@gmail.com>
2026-06-20 15:41:40 +0100

    feat: add spring-boot-starter-actuator dependency

--- services/backend/build.gradle.kts
+++ services/backend/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.swagger.core.v3:swagger-annotations:2.2.28")
